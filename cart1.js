b730cc380dda693eb531d2451038f147a0f9836d
vgarima20 <87186796+vgarima20@users.noreply.github.com>
2021-07-27 21:31:35 +0530

    Add files via upload

---
+++ cart1.js
@@ -0,0 +1,9 @@
+let carts = document.querySelectorAll('.add-cart');
+for (let i=0; i< carts.length; i++){
+    carts[i].addEventListener('click', ()=>{
+        cartNumbers();
+    })
+}
+function cartNumbers(){
+    localStorage.setItem('cartNumbers',1)
+}
